476a688d5b39141b37ec31c298af15105b8ce9c6
LM <laurimeltti@gmail.com>
2022-12-04 12:47:03 +0200

    day4 done

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -1,14 +1,61 @@
+use std::cmp::*;
 
-pub fn get_overlapping_pair_amount(sheet: String) -> i32 { 
-    sheet.lines().map(|line| { 
-        let parts =line.split(",");
-        println!("{:?}", parts)
+pub fn get_covered_pair_amount(sheet: String) -> i32 {
+    return sheet.lines().fold(0, |acc, line| {
+        let (start1, end1, start2, end2) = line_to_tuple(line);
 
+        let should_increment = match start1.cmp(&start2) {
+            Ordering::Equal => true,
+            Ordering::Less => match end1.cmp(&end2) {
+                Ordering::Equal => true,
+                Ordering::Greater => true,
+                Ordering::Less => false,
+            },
+            Ordering::Greater => match end2.cmp(&end1) {
+                Ordering::Equal => true,
+                Ordering::Greater => true,
+                Ordering::Less => false,
+            },
+        };
+
+        let increment_amount = match should_increment {
+            true => 1,
+            false => 0,
+        };
+
+        return acc + increment_amount;
     });
-
-    return 42;
-
 }
+
+pub fn get_overlapping_pair_amount(sheet: String) -> i32 {
+    return sheet.lines().fold(0, |acc, line| {
+        let (start1, end1, start2, end2) = line_to_tuple(line);
+
+        let should_increment = start1 <= end2 && start2 <= end1;
+
+        let increment_amount = match should_increment {
+            true => 1,
+            false => 0,
+        };
+        return acc + increment_amount;
+    });
+}
+
+fn line_to_tuple(line: &str) -> (i32, i32, i32, i32) {
+    let parts = line.split(",").collect::<Vec<&str>>();
+    let (start1, end1) = tuple_from_dash_string(parts[0]);
+    let (start2, end2) = tuple_from_dash_string(parts[1]);
+    return (start1, end1, start2, end2);
+}
+
+fn tuple_from_dash_string(value: &str) -> (i32, i32) {
+    let dash_split = value.split("-").collect::<Vec<&str>>();
+    return (
+        dash_split[0].parse::<i32>().unwrap(),
+        dash_split[1].parse::<i32>().unwrap(),
+    );
+}
+
 // #[cfg(test)]
 // mod tests {
 //     #[test]
