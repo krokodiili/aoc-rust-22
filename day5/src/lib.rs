3434be0ff88a8e75091942fa524181b374cf5904
Lauri Meltti <lauri@vertics.co>
2022-12-09 17:46:34 +0200

    day5 part1 done

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,18 +1,11 @@
 use itertools::Itertools;
 use regex::Regex;
 
-fn strip_string_to_alphabets(stack_string: &str) -> String {
-    let re = Regex::new(r"([A-Z])").unwrap();
-    let result: Vec<&str> = re.find_iter(stack_string).map(|cap| cap.as_str()).collect();
-
-    return result.join("");
-}
-
 pub fn stacks_string_to_vectors(stacks: &str) -> Vec<Vec<char>> {
-    println!("{}", stacks);
     let lines: Vec<Vec<char>> = stacks
         .lines()
-        .map(|line| strip_string_to_alphabets(line).chars().collect())
+        .map(|line| spaces_to_tabs(line))
+        .map(|line| strip_string_to_alphabets_tabs(line).chars().collect())
         .collect();
 
     let grouped_lines = lines.into_iter().fold(vec![vec![]], |mut acc, line| {
@@ -27,18 +20,52 @@ pub fn stacks_string_to_vectors(stacks: &str) -> Vec<Vec<char>> {
         return acc;
     });
 
-    let reversed = grouped_lines
+
+    let reversed_without_tabs = grouped_lines
         .into_iter()
+        .map(|group| {
+         let filtered: Vec<char> = group.iter().filter(|item| **item != '\t').map(|item| item.clone()).collect_vec();
+         return filtered;
+        })
         .map(|mut group| {
             group.reverse();
             return group;
         })
         .collect_vec();
 
-    return reversed;
+    
+    return reversed_without_tabs;
 }
 
-fn execute_move(crate_stacks: Vec<Vec<char>>, command: String) -> Vec<Vec<char>> {
+pub fn join_tops_of_stacks(stacks: Vec<Vec<char>>) -> String {
+    return stacks.clone().iter().fold("".to_string(), |mut acc, stack| { 
+
+        if stack.len() > 0  { 
+            let top_crate = stack.clone().pop().unwrap();
+            acc.push(top_crate);
+            return acc;
+        }
+        return acc;
+
+    });
+
+}
+
+fn spaces_to_tabs(line: &str ) -> String {
+    let re = Regex::new(r"(\s{4})").unwrap();
+    return re.replace_all(line,"\t").to_string();
+}
+
+
+fn strip_string_to_alphabets_tabs(stack_string: String) -> String {
+    let re = Regex::new(r"([A-Z]|\t)").unwrap();
+    let result: Vec<&str> = re.find_iter(&stack_string).map(|cap| {
+        return cap.as_str()
+    }).collect();
+
+    return result.join("");
+}
+pub fn execute_move(crate_stacks: Vec<Vec<char>>, command: String) -> Vec<Vec<char>> {
     let (amount, from, to) = parse_move(command);
 
     let from_stack = crate_stacks[from as usize].clone();
@@ -47,19 +74,17 @@ fn execute_move(crate_stacks: Vec<Vec<char>>, command: String) -> Vec<Vec<char>>
     let split_index = from_stack.len() as i32 - amount;
 
     let (remaining, to_move) = from_stack.split_at(split_index as usize);
-    to_move.iter().for_each(|item| to_stack.push(*item));
+    to_move.iter().rev().for_each(|item| to_stack.push(*item));
 
     let mut result = crate_stacks.clone();
     result[from as usize] = remaining.to_vec();
     result[to as usize] = to_stack;
 
-    println!("result {:?}", to_move);
-
     return result;
 }
 
 fn parse_move(command: String) -> (i32, i32, i32) {
-    let re = Regex::new(r"(\d)").unwrap();
+    let re = Regex::new(r"(\d+)").unwrap();
     let result = re
         .find_iter(&command)
         .map(|cap| cap.as_str().parse::<i32>().unwrap())
@@ -71,6 +96,12 @@ fn parse_move(command: String) -> (i32, i32, i32) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn spaces_to_tabs_works() {
+        let line = "A    B    C";
+        assert_eq!(spaces_to_tabs(line), "A\tB\tC")
+
+    }
 
     #[test]
     fn execute_move_works() {
@@ -85,7 +116,7 @@ mod tests {
     #[test]
     fn strip_stack_string_to_alphabets_works() {
         let stacks = "[Z] [M] [P]";
-        let result = strip_string_to_alphabets(stacks);
+        let result = strip_string_to_alphabets_tabs(stacks.to_string());
         let expected_result = "ZMP";
         assert_eq!(result, expected_result)
     }
