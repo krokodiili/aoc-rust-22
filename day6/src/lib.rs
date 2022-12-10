391a9eef2a9fced299718c8c959842d54e571dfd
LM <laurimeltti@gmail.com>
2022-12-10 11:56:34 +0200

    day6 part2 done

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,16 +1,25 @@
-
 pub fn find_marker_position(datastream: &str) -> i32 {
     let chars: Vec<char> = datastream.chars().collect();
-    let (index_found, _) = chars.iter().enumerate().find(|(index, character)| {
-        let end_index = index + 4 as usize;
-        let sequence = &chars[*index..end_index];
+    let (index_found, _) = chars
+        .iter()
+        .enumerate()
+        .find(|(index, character)| {
+            let end_index = index + 4 as usize;
+            let sequence = &chars[*index..end_index];
+            let seq_string: String = sequence.iter().collect();
 
-        println!("{:?}", sequence);
-        return true;
+            let char_counts = sequence.iter().find(|&&character| {
+                return seq_string.matches(character).count() > 1;
+            });
 
-    }).unwrap();
+            return match char_counts {
+                Some(_) => false,
+                _ => true,
+            };
+        })
+        .unwrap();
 
-    return 5;     
+    return index_found as i32 + 4;
 }
 
 #[cfg(test)]
@@ -20,7 +29,10 @@ mod tests {
     fn it_works() {
         assert_eq!(find_marker_position("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
         assert_eq!(find_marker_position("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-        assert_eq!(find_marker_position("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(
+            find_marker_position("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            10
+        );
         assert_eq!(find_marker_position("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
     }
 }
